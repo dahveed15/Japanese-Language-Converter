3f6d47cda02de9270c4c592efed8f1b5a1b73e39
David Harris <dahveed15@gmail.com>
2019-10-13 17:49:19 -0700

    Setup for romaji and kana

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Textbox from './components/Textbox';
 
 function App() {
   return (
     <div className="App">
-      <p>Hi</p>
+      <h1>English to Japanese Text Converter!</h1>
+      <p>Type the desired hiragana/katakana text in romaji and watch the magic happen!</p>
+      <Textbox />
     </div>
   );
 }
